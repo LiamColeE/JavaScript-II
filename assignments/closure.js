9cbe2c41ef68a7443807c271cb7bfccd9f2e8d74
Jacob Angulo <jacobangulo@hotmail.com>
2019-06-01 13:49:51 -0700

    Revert "Liam edlinger"

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,31 +1,14 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-function add(a,b) {
-  function inner() {
-      console.log(a+b);
-   }
-  return inner();
-}
 
-add(4,6);
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
-var counter = (function () {
-  var num = 0;
-  return () => {
-    num += 1;
-    return num;
-  };
-})();
-
-console.log(counter());
-console.log(counter());
-console.log(counter());
-console.log(counter());
-
+const counter = () => {
+  // Return a function that when invoked increments and returns a counter variable.
+};
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
